341901b732ddba50c2d66b6a942a0092e72b922f
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-21 09:52:12 +0300

    1

--- NullableTypeExtensions.cs
+++ NullableTypeExtensions.cs
@@ -6,8 +6,17 @@ namespace PadawansTask10
     {
         public static bool IsNull(this object variable)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (variable is string)
+                return false;
+            if (variable is char)
+                return false;
+            if (variable is bool)
+                return false;
+            if (variable is double[])
+                return false;
+
+            return (variable as double?) == null;
+
         }
     }
 }
